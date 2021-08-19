41ed36ea1dd04916f57ac0db41495e751d5d308f
SanoberParveen <86728558+SanoberParveen@users.noreply.github.com>
2021-08-19 14:38:34 +0530

    Delete lib directory

--- lib/question.dart
+++
@@ -1,9 +0,0 @@
-class Question {
-  String questionText = '';
-  bool questionAnswer = false;
-
-  Question({required String q, required bool a}) {
-    questionText = q;
-    questionAnswer = a;
-  }
-}
